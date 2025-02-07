c610034850f0cfc441aefff8ddb52835417a99e2
hugo lopez vidal <hlopezvidal.ieu2023@student.ie.edu>
2025-02-07 10:29:01 +0100

    homework 1 updated

--- Homework 1.py
+++ Homework 1.py
@@ -1,3 +1,9 @@
 print("Homework 1 completed")
 print (10+5)
+#calculation of next years gdp for country x, using this years gdp, and annual growth rate
+gdp = 5000  # Initial GDP in billions
+growth_rate = 0.03  # 3% growth rate
+new_gdp = gdp * (1 + growth_rate)  # Calculate next year's GDP
+print(f"Next year's GDP: ${new_gdp:.2f} billion")
+
 
